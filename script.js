b304eba07af6cd94ce556936c809669949336efa
Celena Handojo <cmhandojo@gmail.com>
2018-07-26 14:11:10 -0700

    beginning edits of jquery div fade

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ $(function(){
   $("#page")
     .fadeIn(800)
     .removeClass("hidden");
+
+  $(".namecard").addClass("active");
+
+    $("a.project-weblink").click(function(){
+      $(".projects").toggleClass("hidden");
+    });
 });
