5084157307a762d1c73a2eacaaaf548fec3f4374
Celena Handojo <cmhandojo@gmail.com>
2018-07-28 15:51:47 -0700

    designed sections and page

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ $(function(){
     .fadeIn(800)
     .removeClass("hidden");
 
-    $("a.project-weblink").on('click', event => {
-      $(".projects")
-      .fadeToggle("hidden");
-
-
-      $(".nametag")
-      .toggleClass("hidden");
-    });
+//    $("a.project-weblink").on('click', event => {
+//      $(".projects")
+//      .fadeToggle("hidden");
+//
+//
+//      $(".nametag")
+//      .toggleClass("hidden");
+//    });
 
     console.log(" (\\_/) \n (o.o) \n (___)0 \n You found me");
 
