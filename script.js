ea362edfa3dec20981992e221b73569bf602805f
Celena Handojo <cmhandojo@gmail.com>
2018-07-28 19:51:09 -0700

    centered divs and added toggle effects for all divs

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ $(function(){
     .fadeIn(800)
     .removeClass("hidden");
 
-    $("a.project-weblink").on('click', event => {
-      $(".projects")
-      .fadeToggle("hidden");
-
-
-      $(".namecard")
-      .toggleClass("hidden");
+    $("#link-1,#link-2,#link-3").click(function () {
+        $(".section").eq($(this).index()).fadeToggle("hidden");
+        $('.section:not(":eq('+$(this).index()+')")').fadeOut();
+        $(".namecard").addClass("hidden");
+        $(".space").addClass("hidden");
     });
+
+
+//    $("a.project-weblink").on('click', event => {
+//      $(".projects")
+//      .fadeToggle("hidden");
+//
+//
+//      $(".namecard")
+//      .toggleClass("hidden");
+//    });
+
+
+
+
 
     console.log(" (\\_/) \n (o.o) \n (___)0 \n You found me");
 
