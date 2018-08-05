0349fe50c0c79321e707e1b9a0bf68c69b8084e8
Celena Handojo <cmhandojo@gmail.com>
2018-08-05 13:09:14 -0700

    added email address

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
           <h2 class="title">Let's Work Together</h2>
           <div class="contact-section">
           <p class="contactme">If you have a cool project in mind or like my work, send me a message.    </p>
-          <p>cmhandojo@gmail.com</p>
+          <p class="contactme"><i class="far fa-envelope"></i> cmhandojo@gmail.com</p>
 
         </div>
 
@@ -110,7 +110,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
     <script src="https://cdn.rawgit.com/nnattawat/flip/master/dist/jquery.flip.min.js"></script>
-    <script src="script.js"></script> 
+    <script src="script.js"></script>
 
   </body>
 </html>
