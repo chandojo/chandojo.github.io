659811d37434f88ba996b993049cb6e3db6ad366
chandojo <celena.handojo+github@gmail.com>
2020-06-24 10:46:32 -0700

    Revert "simplified webpage (#4)"
    This reverts commit 8b021a04458e88a9dbca96781dc6f4f85d3e01c0.

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Smooth scrolling
-$("a").on('click', function(event) {
+  $("a").on('click', function(event) {
     if (this.hash !== "") {
-        event.preventDefault();
+      event.preventDefault();
 
-        var hash = this.hash;
+      var hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function() {
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
 
-            window.location.hash = hash;
-        });
+        window.location.hash = hash;
+      });
     }
-});
+  });
 
 
-$(function() {
-    var white = document.createElement("div");
-    white.classList.add("white");
-    document.getElementById("bucket").appendChild(white);
+$(function(){
+  var white = document.createElement("div");
+  white.classList.add("white");
+  document.getElementById("bucket").appendChild(white);
 
-    $(".white").append("(\\_/)<br>(o.o)<br>(___)0 <br>");
+  $(".white").append("(\\_/)<br>(o.o)<br>(___)0 <br>");
 
-    console.log(" (\\_/) \n (o.o) \n (___)0 \n Following the white rabbit...");
+  console.log(" (\\_/) \n (o.o) \n (___)0 \n Following the white rabbit...");
 });
